4f1173c20da122cb0392e7349dfa43683e753068
yuke brilliant <yukebrilliant@gmail.com>
2021-08-14 20:57:29 +0700

    Remove headers and axios config

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const URI = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScOwnlOEdG2z7SVJLjd4LE
 const draftResponse = '[[[null,762512528,[' + config.nama + '],0],[null,986366620,["' + config.absen + '"],0],[null,1831653095,["' + config.kelas + '"],0],[null,1107982372,[' + dateFormat + '],0],[null,1570564577,["' + config.keterangan + '"],0],[null,1856075124,["' + config.kelasDetail + '"],0]],null,"4734432169028665497"]'
 '[[[null,762512528,[' + config.nama + '],0],[null,986366620,["' + config.absen + '"],0],[null,1831653095,["' + config.kelas + '"],0],[null,1107982372,[' + dateFormat + '],0],[null,1570564577,["' + config.keterangan + '"],0],[null,1856075124,["' + config.kelasDetail + '"],0]],null,"4734432169028665497"]';
 
-const firstData = qs.stringify({
+console.log("\x1b[36m%s\x1b[0m", "Sedang mengisi presensi....");
+axios.post(URI, qs.stringify({
     'entry.1107982372_year': year,
     'entry.1107982372_month': ("0" + month).slice(-2),
     'entry.1107982372_day': day,
@@ -26,26 +27,17 @@ const firstData = qs.stringify({
     'pageHistory': '0',
     'fbzx': '435254377647',
     'continue': '1'
-});
-
-const nextData = qs.stringify({
-    'entry.1856075124': config.kelasDetail,
-    'entry.1856075124_sentinel': '',
-    'fvv': '1',
-    'draftResponse': draftResponse,
-    'pageHistory': '0,1',
-    'fbzx': '435254377647'
-});
-
-
-console.log("\x1b[36m%s\x1b[0m", "Sedang mengisi presensi....");
-
-axios.post(URI, firstData)
+}))
     .then(() => {
-        axios.post(URI, nextData)
+        axios.post(URI, qs.stringify({
+            'entry.1856075124': config.kelasDetail,
+            'entry.1856075124_sentinel': '',
+            'fvv': '1',
+            'draftResponse': draftResponse,
+            'pageHistory': '0,1',
+            'fbzx': '435254377647'
+        }))
             .then(() => {
                 console.log("\x1b[32m%s\x1b[0m", "Sukses mengisi presensi!");
             });
     });
-
-
